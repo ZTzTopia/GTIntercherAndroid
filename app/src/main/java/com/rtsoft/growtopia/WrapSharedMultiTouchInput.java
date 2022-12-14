15f29ceb2ee9ea26707ec40ac9705b2e3ed759b8
ZTz <36158219+ZTzTopia@users.noreply.github.com>
2022-12-14 19:56:20 +0700

    cleanup WrapSharedMultiTouchInput.java

--- app/src/main/java/com/rtsoft/growtopia/WrapSharedMultiTouchInput.java
+++ app/src/main/java/com/rtsoft/growtopia/WrapSharedMultiTouchInput.java
@@ -3,21 +3,21 @@ package com.rtsoft.growtopia;
 import android.view.MotionEvent;
 
 class WrapSharedMultiTouchInput {
-    private SharedMultiTouchInput mInstance;
-
     static {
         try {
             Class.forName("com.rtsoft.growtopia.SharedMultiTouchInput");
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
+
+    private SharedMultiTouchInput mInstance;
 
     public static boolean OnInput(MotionEvent motionEvent) {
         return SharedMultiTouchInput.OnInput(motionEvent);
     }
 
+    /* calling here forces class initialization */
     public static void checkAvailable(SharedActivity sharedActivity) {
         SharedMultiTouchInput.init(sharedActivity);
     }
